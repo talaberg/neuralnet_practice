246077963fd5fc196686331d7fa79e8fd5c32a5d
talaberg <talaber@vipmail.hu>
2018-12-02 23:25:25 +0100

    - Learning v2 - Output scaling

--- src/neuralnet/Main.java
+++ src/neuralnet/Main.java
@@ -1,19 +1,19 @@
 package neuralnet;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 public class Main {
 	public static final int TRAINING_DATA_SIZE = 10000;
-	public static final int TEST_DATA_SIZE = 10
-			;
-	public static final int INPUT_SIZE = 3
-			;
-	public static final float LEARNING_STEP = 0.1f;
-	public static final float ACTIVATION_SAT_LIMIT = 1.73f;
+	public static final int TEST_DATA_SIZE = 10;
+	public static final int INPUT_SIZE = 3;
+	/*public static final int TRAINING_DATA_SIZE = 3;
+	public static final int TEST_DATA_SIZE = 4;
+	public static final int INPUT_SIZE = 2;*/
+
+	public static final float LEARNING_STEP = 0.2f;
+	public static final float ACTIVATION_SAT_LIMIT = 1.0f;
+	public static final float BIAS_HIDDEN_LAYER = 1.0f;
+	public static final float BIAS_OUTPUT_LAYER = 1.0f;
 
 	public static class Node
 	{
@@ -65,15 +65,17 @@ public class Main {
 		public Neuron[] hiddenLayer;
 		public int hiddenLayerSize;
 		public float[] hiddenLayerError;
-		public Output output;
+		public Output outputLayer;
 		public float outputLayerError;
+		float output;
+		float outputRange;
 
 		public NeuralNet(int inputSize, int hiddenLayerSize)
 		{
 			this.inputs = new Input[inputSize]; this.inputSize = inputSize;
 			this.hiddenLayer = new Neuron[hiddenLayerSize]; this.hiddenLayerSize = hiddenLayerSize;
-			
-			inputLayerError =  new float[inputSize];
+
+			inputLayerError =  new float[hiddenLayerSize];
 			hiddenLayerError = new float[hiddenLayerSize];
 			outputLayerError = 0;
 
@@ -86,69 +88,64 @@ public class Main {
 			{
 				hiddenLayer[i] = new Neuron(inputSize);
 			}
-			output = new Output(hiddenLayerSize);
+			outputLayer = new Output(hiddenLayerSize);
 
 			for(int i = 0; i < hiddenLayerSize; i++) // initialize with random number between (-0.5; 0.5)
 			{
 				for(int j = 0; j < inputSize; j++) {
-					hiddenLayer[i].bias[j] = 0;
+					hiddenLayer[i].bias[j] = BIAS_HIDDEN_LAYER;
 					hiddenLayer[i].weight[j] = ((float)Math.random() * 2.0f - 1 ) * ACTIVATION_SAT_LIMIT;
 				}
 			}
 			for(int i = 0; i < hiddenLayerSize; i++)
 			{
-				output.bias[i] = 0;
-				output.weight[i] = ((float)Math.random() * 2.0f - 1 ) * ACTIVATION_SAT_LIMIT;
+				outputLayer.bias[i] = BIAS_OUTPUT_LAYER;
+				outputLayer.weight[i] = ((float)Math.random() * 2.0f - 1 ) * ACTIVATION_SAT_LIMIT;
 			}
 		}
 
-		public void Learn(float[][] trainingData, float[] trainingOutput)
+		public void Learn(float[][] trainingData, float[] trainingOutput, float range)
 		{
+			outputRange = range;
 			try
 			{
 				for(int i = 0; i < TRAINING_DATA_SIZE; i++)
 				{
-					// Calculate output
+					// Calculate output (forward pass)
 					Execute(trainingData[i], inputSize);
 
 					// Calculate signal errors ------------------------------------------------------------------------------------
 					// Calculate error for the output layer
-					float diff = (trainingOutput[i] - output.activation);
-					outputLayerError = diff*diff * output.AcitvationFunctionDeriv(output.activation);
-					
-					float sumError = 0;
+					float dOut = -2.0f * ((trainingOutput[i] / outputRange) - outputLayer.activation) * outputLayer.AcitvationFunctionDeriv(outputLayer.activation);
+
 					// Calculate error for the hidden layer
 					for(int j = 0; j < hiddenLayerSize; j++)
 					{
-						sumError = output.weight[j]* outputLayerError;
-						
-						hiddenLayerError[j] = hiddenLayer[j].AcitvationFunctionDeriv(hiddenLayer[j].activation) * sumError;
+						hiddenLayerError[j] = dOut * hiddenLayer[j].activation;
 					}
+
 					// Calculate error for the input layer
-					for(int j = 0; j < inputSize; j++)
-					{
-						sumError = 0;
-						for(int k = 0; k < hiddenLayerSize; k++)
-						{
-							sumError += hiddenLayer[k].weight[j]* hiddenLayerError[k];
-						}
-						inputLayerError[j] = inputs[j].AcitvationFunctionDeriv(inputs[j].activation) * sumError;
-					}
-					// Backpropagate errors ------------------------------------------------------------------------------------
-					
-					for(int j = 0; j < hiddenLayerSize; j++)
-					{
-						output.weight[j] = output.weight[j] + hiddenLayerError[j] * LEARNING_STEP;
-					}
-					
 					for(int j = 0; j < hiddenLayerSize; j++)
 					{
 						for(int k = 0; k < inputSize; k++)
 						{
-							hiddenLayer[j].weight[k] = hiddenLayer[j].weight[k] + inputLayerError[j] * LEARNING_STEP;
+							inputLayerError[j] = dOut * outputLayer.weight[j] * hiddenLayer[j].AcitvationFunctionDeriv(hiddenLayer[j].activation);
 						}
 					}
-					
+					// Backpropagate errors ------------------------------------------------------------------------------------
+
+					for(int j = 0; j < hiddenLayerSize; j++)
+					{
+						outputLayer.weight[j] = outputLayer.weight[j] - hiddenLayerError[j] * LEARNING_STEP;
+					}
+
+					for(int j = 0; j < hiddenLayerSize; j++)
+					{
+						for(int k = 0; k < inputSize; k++)
+						{
+							hiddenLayer[j].weight[k] = hiddenLayer[j].weight[k] - inputLayerError[j] * inputs[k].activation * LEARNING_STEP;
+						}
+					}
 				}
 			}
 			catch(Exception e){
@@ -167,27 +164,30 @@ public class Main {
 				hiddenLayer[i].Execute(inputs, inputSize);
 			}
 
-			output.Execute(hiddenLayer, hiddenLayerSize);	// run output layer neuron
-			return output.activation;
+			outputLayer.Execute(hiddenLayer, hiddenLayerSize);	// run output layer neuron
+
+			output = outputLayer.activation * outputRange;
+			return output;
 		}
+	}
+
+	public static class MinMax
+	{
+		public float Min; public float Max;
+		public MinMax() {Min = Float.MAX_VALUE; Max = -Float.MAX_VALUE; }
 	}
 
 	public static class InputData
 	{
 		public float[][] trainingData;
 		public float[] trainingOutput;
+		float outputRange;
 		public float[][] testData;
 		public InputData(int trainingDataSize, int testDataSize, int dataInputSize) { testData = new float[testDataSize][dataInputSize];
 			trainingData = new float[trainingDataSize][dataInputSize]; trainingOutput = new float[trainingDataSize]; }
 	}
-	
-	public static class MinMax
-	{
-		public float Min; public float Max;
-		public MinMax() {Min = Float.MAX_VALUE; Max = -Float.MAX_VALUE; }
-	}
-	
-	public static MinMax[] getMinMax(float[][] data)
+
+	public static MinMax[] getInputMinMax(float[][] data)
 	{
 		MinMax[] values = new MinMax[INPUT_SIZE];
 		for (int j = 0; j < INPUT_SIZE; j++)
@@ -208,8 +208,24 @@ public class Main {
 				}
 			}
 		}
-		
 		return values;
+	}
+
+	public static float getOutputRange(float[] data)
+	{
+		MinMax mm = new MinMax();
+		for(int i = 0; i < TRAINING_DATA_SIZE; i++)
+		{
+			if(data[i] > mm.Max)
+			{
+				mm.Max = data[i];
+			}
+			if(data[i] < mm.Min)
+			{
+				mm.Min = data[i];
+			}
+		}
+		return mm.Max - mm.Min;
 	}
 
 	public static InputData ReadData()
@@ -250,9 +266,10 @@ public class Main {
 			scanner.close();
 			System.err.println("Invalid input format!");
 		}
-		
+
 		// Normalize inputs
-		/*MinMax[] minmax = getMinMax(data.trainingData);
+		MinMax[] minmax = getInputMinMax(data.trainingData);
+
 		for(int i = 0; i < TRAINING_DATA_SIZE; i++)
 		{
 			for (int j = 0; j < INPUT_SIZE; j++)
@@ -260,15 +277,17 @@ public class Main {
 				data.trainingData[i][j] = (data.trainingData[i][j] - minmax[j].Min) / (minmax[j].Max - minmax[j].Min) * ACTIVATION_SAT_LIMIT;
 			}
 		}
-		
+
 		for(int i = 0; i < TEST_DATA_SIZE; i++)
 		{
 			for (int j = 0; j < INPUT_SIZE; j++)
 			{
 				data.testData[i][j] = (data.testData[i][j] - minmax[j].Min) / (minmax[j].Max - minmax[j].Min) * ACTIVATION_SAT_LIMIT;
 			}
-		}*/
-		
+		}
+
+		data.outputRange = getOutputRange(data.trainingOutput);
+
 		return data;
 	}
 
@@ -280,11 +299,11 @@ public class Main {
 		InputData data = ReadData();
 
 		// Create neural network
-		NeuralNet nn = new NeuralNet(INPUT_SIZE, INPUT_SIZE - 1);
+		NeuralNet nn = new NeuralNet(INPUT_SIZE, INPUT_SIZE);
 		Float[] outputs = new Float[TEST_DATA_SIZE];
 
 		//Train graph
-		nn.Learn(data.trainingData, data.trainingOutput);
+		nn.Learn(data.trainingData, data.trainingOutput, data.outputRange);
 
 		try
 		{
